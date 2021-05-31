7a66cdfa727e6ce169c8cebd3930f4a63efe19eb
gustavo L <gustalondon2016@gmail.com>
2021-05-31 16:02:09 -0500

    add App.js para manejar rutas usando router y switch

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Register from '../pages/Register'
+import Login from '../pages/Login'
+
+
+function App() {
+    return (
+        <BrowserRouter>
+        <Switch>
+            <Route exact path="/register" component={ Register }/>
+            <Route exact path="/login" component={Login}/>
+        </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default App
